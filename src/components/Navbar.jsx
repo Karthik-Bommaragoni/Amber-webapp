33e8eba40a1e832839a2d5a855f9e14366cc4a03
Karthik <b.karthik1357@gmail.com>
2026-04-03 15:54:10 +0530

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
+
 function Navbar() {
   const { user, logout } = useContext(AuthContext);
 
